44da3327d6ee5b6d1fa967e22e0a2f91a3266db2
Jason Pham <j.23pham@gmail.com>
2019-10-14 17:01:56 +1100

    Added header.js

---
+++ header.js
@@ -0,0 +1,7 @@
+function initHeader () {
+    let header = document.createElement("header");
+    let title = document.createElement("h1");
+    title.innerText = "Hello, World";
+    header.appendChild(title);
+    document.getElementById("root").appendChild(header);
+}
