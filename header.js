43aeb62f1763531ffceef55b3a7724072f526b97
Jason Pham <j.23pham@gmail.com>
2019-10-14 22:28:33 +1100

    Updated header.js

--- header.js
+++ header.js
@@ -1,6 +1,12 @@
 function initHeader () {
     let navigationBar = document.createElement("nav");
+    let logo = document.createElement("a");
+    
     navigationBar.className = "navbar navbar-expand-lg navbar-light bg-light";
+    logo.className = "navbar-brand";
+    logo.href = "#";
+    logo.innerText = "wsiw";
+    
     let header = document.createElement("header");
     let title = document.createElement("h1");
     title.innerText = "Hello, World";
