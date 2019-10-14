7a9b71ea68d7f5e4b3a03779fa3fe0d433dfd206
Jason Pham <j.23pham@gmail.com>
2019-10-14 22:03:01 +1100

    Testing header

--- header.js
+++ header.js
@@ -1,9 +1,11 @@
 function initHeader () {
+    let navigationBar = document.createElement("nav");
+    navigationBar.className = "navbar navbar-expand-lg navbar-light bg-light";
     let header = document.createElement("header");
     let title = document.createElement("h1");
     title.innerText = "Hello, World";
     header.appendChild(title);
-    document.getElementById("root").appendChild(header);
+    document.getElementById("root").appendChild(navigationBar);
 }
 
 export default initHeader;
