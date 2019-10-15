228641063251a507b354de2a104664e100571cee
Jason Pham <j.23pham@gmail.com>
2019-10-15 16:09:53 +1100

    Updated header

--- src/header.js
+++ src/header.js
@@ -15,13 +15,25 @@ function createLogo(parentNode) {
     parentNode.appendChild(logo);
 }
 
-// Function that creates collapsable navigation bar for mobile viewing
+// Function that enables collapsability for navigation bar for mobile viewing
 function mobileNavigationBar(parentNode){
-    let mobileNavigationBar = document.createElement("button");
-    mobileNavigationBar.className = "navbar-toggler";
-    mobileNavigationBar.type = "button";
+    // let mobileNavigationBar = document.createElement("button");
+    let mobileNavigationBar = createButton("navbar-toggler");
+    // mobileNavigationBar.className = "navbar-toggler";
+    // mobileNavigationBar.type = "button";
     mobileNavigationBar.setAttribute("data-toggle", "collapse");
     parentNode.appendChild(mobileNavigationBar);
+    let navBarIcon = document.createElement("span");
+    navBarIcon.className = "navbar-toggler-icon";
+    mobileNavigationBar.appendChild(navBarIcon);
+}
+
+// Function that creates button base on class type 
+
+function createButton(classType) {
+    let newButton = document.createElement("button");
+    newButton.type = "button";
+    newButton.className = classType;
 }
 
 export default initHeader;
