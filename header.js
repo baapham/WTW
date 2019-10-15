a1155ef72780acaff49b81ba8609dac2b5655d03
Jason Pham <j.23pham@gmail.com>
2019-10-15 13:01:47 +1100

    Updated header.js

--- header.js
+++ header.js
@@ -1,14 +1,26 @@
 function initHeader () {
     let navigationBar = document.createElement("nav");
-    let logo = document.createElement("a");
-    
     navigationBar.className = "navbar navbar-expand-lg navbar-light bg-light";
+    createLogo(navigationBar);
+
+    document.getElementById("root").appendChild(navigationBar);
+}
+
+// Function that creates logo and appends it to relevant node
+function createLogo(parentNode) {
+    let logo = document.createElement("a");
     logo.className = "navbar-brand";
     logo.href = "#";
     logo.innerText = "wsiw";
-    navigationBar.appendChild(logo);
-    
-    document.getElementById("root").appendChild(navigationBar);
+    parentNode.appendChild(logo);
+}
+
+// Function that creates collapsable navigation bar for mobile viewing
+function mobileNavigationBar(parentNode){
+    let mobileNavigationBar = document.createElement("button");
+    mobileNavigationBar.className = "navbar-toggler";
+    mobileNavigationBar.type = "button";
+    mobileNavigationBar.dataToggle = "collapse";
 }
 
 export default initHeader;
